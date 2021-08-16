fb1c5b7adf0364ed07793bb9bd68e853525a9856
danielvtfs <danielvtfs@gmail.com>
2021-08-16 18:22:30 -0300

    ajustes saiba mais

--- src/pages/SaibaMais/SaibaMais.js
+++ src/pages/SaibaMais/SaibaMais.js
@@ -1,15 +1,17 @@
 import './styles.css';
 function SaibaMais() {
   return (
-    <div className="container-saiba">
-      <h1 className="titulo">ART POINT EVENTOS</h1>
-      <p>Somos a ART POINT EVENTOS.</p>
-      <p>Trabalhamos com Projetos de SONORIZAÇÃO, ILUMINAÇÃO e CENOGRAFIA.</p>
-      <p>
-        Fornecemos toda a estrutura para seu evento: Palcos, Tendas,
-        Arquibancadas, Iluminação, Telão de Led, Som, Decoração, Buffet, Box
-        Truss e muito mais...
-      </p>
+    <div className="container">
+      <div>
+        <h1 className="titulo">ART POINT EVENTOS</h1>
+        <p>Somos a ART POINT EVENTOS.</p>
+        <p>Trabalhamos com Projetos de SONORIZAÇÃO, ILUMINAÇÃO e CENOGRAFIA.</p>
+        <p>
+          Fornecemos toda a estrutura para seu evento: Palcos, Tendas,
+          Arquibancadas, Iluminação, Telão de Led, Som, Decoração, Buffet, Box
+          Truss e muito mais...
+        </p>
+      </div>
     </div>
   );
 }
