c381dd1865ff1e89f4d1870e6ddd567af9ce111f
danielvtfs <danielvtfs@gmail.com>
2021-09-10 07:04:44 -0300

    Corrigindo whatsapp

--- src/components/WhatsApp/Whatsapp.js
+++ src/components/WhatsApp/Whatsapp.js
@@ -3,7 +3,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 function WhatssApp() {
   return (
     <a
-      href="https://web.whatsapp.com/send?phone=5521970448540"
+      href="https://web.whatsapp.com/send?phone=+5521970448540"
       className="whatsapp_float"
       target="_blank"
       rel="noopener noreferrer"
